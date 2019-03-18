9d854dc57396d3c09e88f5288c1faeacfde49d16
Kate Glazko <katglazko@gmail.com>
2019-03-18 10:59:08 -0700

    new

--- app/src/androidTest/java/org/mozilla/tv/firefox/ui/BasicNavigationTest.kt
+++ app/src/androidTest/java/org/mozilla/tv/firefox/ui/BasicNavigationTest.kt
@@ -27,8 +27,11 @@ class BasicNavigationTest {
 
     /* ktlint-disable no-blank-line-before-rbrace */ // This imposes unreadable grouping.
     @Test
+<<<<<<< HEAD
     // This test fails on emulators. CI is run on physical devices on BitBar, so this is acceptable.
     //
+=======
+>>>>>>> new
     // This test verifies (among other things) that the URL bar is filled with the page title after opening a URL.
     //
     // On emulators, the URL bar shows various assets instead of page titles (#1444) whenever Turbo mode is enabled.
